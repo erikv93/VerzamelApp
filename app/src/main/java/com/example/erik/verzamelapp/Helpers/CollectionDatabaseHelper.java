ee851aae67a7af9c4ef908a0a04d92914ac317ed
Erik Voors <erik.voors@hva.nl>
2016-03-03 12:43:01 +0100

    Added multiple files

---
+++ app/src/main/java/com/example/erik/verzamelapp/Helpers/CollectionDatabaseHelper.java
@@ -0,0 +1,7 @@
+package com.example.erik.verzamelapp.Helpers;
+
+/**
+ * Created by Erik on 3-3-2016.
+ */
+public class CollectionDatabaseHelper {
+}
--- app/src/main/java/com/example/erik/verzamelapp/MainActivity.java
+++ app/src/main/java/com/example/erik/verzamelapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        final ListView listView = (ListView)findViewById(R.id.listView);
     }
 
     @Override
